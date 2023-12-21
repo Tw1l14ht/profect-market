14d1fe74d7605919bbf5fb03051ec8ebe18645e9
Rozovika Ivan <ivanrozovika@yandex.ru>
2023-12-21 15:15:06 +0300

    fix links

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -4,41 +4,51 @@ import check from "../image/check.svg";
 import errImg from "../image/errImg.svg";
 import deleteProf from "../image/deleteProf.svg";
 import plusProfs from "../image/plusProfs.svg";
+import {  Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 function CreateProject() {
+    const navigate = useNavigate();
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        navigate("/projects", { replace: true });
+    }
+
     return (
         <div className="new-proj">
             <h1 className="new-proj__head">Создание проекта</h1>
-            <div className="new-proj__content">
-                <h2 className="new-proj__title new-proj__title-proj">Название проекта</h2>
-                <input type="text" className="new-proj__title-input" placeholder="Введите название..." />
-                <h2 className="new-proj__title new-proj__title-photo">Фото проекта</h2>
-                <label for="photoInput" className="upload__photo">
-                    <p className="upload__photo-title">Загрузить изображение</p>
-                    <img className="upload__photo-logo" src={uploadLogo} />
-                </label>
-                <input id="photoInput" type="file" className="new-proj__photo-input" placeholder="" />
-                <h2 className="new-proj__title new-proj__title-about">О проекте</h2>
-                <textarea className="new-proj__about-input" placeholder="Введите описание"></textarea>
-                <h2 className="new-proj__title  ">Занятые роли</h2>
-                <div className="card__profs">
-                    <div className="card__prof-descrb card__prof-descrb_newProj">
-                        <img className="card__profCheck" src={check} alt=""></img>
-                        <p className="card__prof-name">Title</p>
-                        <img src={deleteProf} alt="#" className="card__deleteImg" />
+            <form noValidate onSubmit={handleSubmit} name="proj__form" id="proj__form">
+                <fieldset className="new-proj__content">
+                    <h2 className="new-proj__title new-proj__title-proj">Название проекта</h2>
+                    <input type="text" className="new-proj__title-input" placeholder="Введите название..." />
+                    <h2 className="new-proj__title new-proj__title-photo">Фото проекта</h2>
+                    <label for="photoInput" className="upload__photo">
+                        <p className="upload__photo-title">Загрузить изображение</p>
+                        <img className="upload__photo-logo" src={uploadLogo} />
+                    </label>
+                    <input id="photoInput" type="file" className="new-proj__photo-input" placeholder="" />
+                    <h2 className="new-proj__title new-proj__title-about">О проекте</h2>
+                    <textarea className="new-proj__about-input" placeholder="Введите описание"></textarea>
+                    <h2 className="new-proj__title  ">Занятые роли</h2>
+                    <div className="card__profs">
+                        <div className="card__prof-descrb card__prof-descrb_newProj">
+                            <img className="card__profCheck" src={check} alt=""></img>
+                            <p className="card__prof-name">Title</p>
+                            <img src={deleteProf} alt="#" className="card__deleteImg" />
+                        </div>
+                        <img className="card__plusProfs" src={plusProfs} />
                     </div>
-                    <img className="card__plusProfs" src={plusProfs} />
-                </div>
-                <h2 className="new-proj__title new-proj__title-req">Вакантные роли</h2>
-                <div className="card__profs">
-                    <div className="card__prof-descrb card__prof-descrb_req card__prof-descrb_newProj">
-                        <img className="card__profCheck" src={errImg} alt=""></img>
-                        <p className="card__prof-name">Title</p>
-                        <img src={deleteProf} alt="#" className="card__deleteImg" />
+                    <h2 className="new-proj__title new-proj__title-req">Вакантные роли</h2>
+                    <div className="card__profs">
+                        <div className="card__prof-descrb card__prof-descrb_req card__prof-descrb_newProj">
+                            <img className="card__profCheck" src={errImg} alt=""></img>
+                            <p className="card__prof-name">Title</p>
+                            <img src={deleteProf} alt="#" className="card__deleteImg" />
+                        </div>
+                        <img className="card__plusProfs" src={plusProfs} />
                     </div>
-                    <img className="card__plusProfs" src={plusProfs} />
-                </div>
-            </div>
+                    <button  id="proj__btn" className="new__proj-btn" type="submit">Создать</button>
+                </fieldset>
+            </form>
         </div>
     );
 }
--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -1,13 +1,20 @@
 import React from "react";
+import {  Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 function NewProject({handleCreateProjClick}) {
+    const navigate = useNavigate();
+    function handleClick() {
+        navigate("/projects/newProject", { replace: true });
+    }
+
     return (
         <div className="projects">
             <div className="projects__links">
                 <a href="" className="projects_link-proj">Мои проекты</a>
                 <a href="" className="projects_link-req">Заявки</a>
+                <a href="" className="projects_link-like">Избранное</a>
             </div>
-            <button type="button" onClick={handleCreateProjClick} className="projects__button">Создать проект</button>
+            <button type="button" onClick={handleClick} className="projects__button">Создать проект</button>
         </div>
     );
 }
